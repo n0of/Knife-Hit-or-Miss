423ce622d6905f4e2f5c4b9314e1dc72722e3d74
n0of <32220751+n0of@users.noreply.github.com>
2021-02-03 02:26:15 +0500

    Relocated to folder

--- StaticVariables.cs
+++
@@ -1,8 +0,0 @@
-﻿public static class StaticVariables
-{
-    public static int StuckToWin { get; } = 10;
-    public static int MaxInitStuck { get; } = 3;
-    public static float KunaiMargin { get; } = 0.5f;
-    public static string LogObjectName { get; } = "Log";
-    public static string GetKunaiName(int id) => $"Kunai{id}";
-}
